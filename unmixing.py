3019032d60bcbef016a376d8e3b0d1b976c4c104
dpoteryayev <dpoteryayev@gmail.com>
2025-02-21 16:27:06 +0100

    Wrote unmixing script to try it when Franklin will be free

--- unmixing.py
+++ unmixing.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-import numpy as np
+# import numpy as np
 import jax
 print("JAX devices: ", jax.devices())
 import ml_confs
@@ -8,13 +8,14 @@ import optax
 import orbax.checkpoint as ocp
 import xarray as xr
 from etils import epath
-from flax.training.common_utils import stack_forest
+# from flax.training.common_utils import stack_forest
 from flax.training.train_state import TrainState
-from tensorboardX import SummaryWriter
+# from tensorboardX import SummaryWriter
 
 from spectraformer.input_pipeline import batch_sampler, preprocess_dataset
 from spectraformer.model import SpectraFormer
-from spectraformer.train import train_epoch, validation_epoch
+# from spectraformer.train import train_epoch, validation_epoch
+from spectraformer.inference import predict #, plot_results
 
 jax.config.update("jax_debug_nans", True)
 
@@ -39,31 +40,29 @@ configsdir.mkdir(parents=True, exist_ok=True)
 config_file_name = f"configs_{model_tag}.yaml"
 config_file_path = configsdir / config_file_name
 
+mixdir = datadir / "mixtures"
+mixdir.mkdir(parents=True, exist_ok=True)
+
 def load_model(
-    model_tag: str,
+    configs,
     dataset
     ):
-    ckpt_manager = ocp.CheckpointManager(
-        ckptdir + 'spectraformer:' + model_tag,
-        item_handlers=ocp.StandardCheckpointHandler(),
-    )
-    configs = ml_confs.from_dict(ckpt_manager.metadata())
+    
     mask_windows = list(
         zip(configs.masked_interval_starts, configs.masked_interval_ends)
     )
+    
     dummy_example = next(batch_sampler(dataset, mask_windows, batch_size=1))
-    # Re-initialize model based on loaded configs
+    
     model = SpectraFormer(
         num_heads=configs.num_heads,
         num_layers=configs.num_layers,
         embedding_dim=configs.embedding_dim,
     )
-
-    # RNG Keys
+    
     root_key = jax.random.key(seed=configs.root_rng_seed)
     main_key, params_key, dropout_key = jax.random.split(key=root_key, num=3)
-
-    # Model Initialization
+    
     variables = model.init(
         params_key,
         dummy_example["masked_spectra"][0],
@@ -77,14 +76,41 @@ def load_model(
         params=variables["params"],
         tx=optax.adam(configs.learning_rate),
     )
+    
+    ckpt_options = ocp.CheckpointManagerOptions(max_to_keep=5, read_only=True)
+    
+    ckpt_manager = ocp.CheckpointManager(
+        ckptdir / configs.tag,
+        options=ckpt_options,
+        item_handlers=ocp.StandardCheckpointHandler(),
+        metadata=configs.to_dict(),
+    )
+    
+    # After initialization remove the dummy file
+    if epath.Path(ckptdir / configs.tag / ".tmp").exists():
+        epath.Path(ckptdir / configs.tag / ".tmp").rmtree()
+    
     # Restore checkpoint
     state = ckpt_manager.restore(
         ckpt_manager.latest_step(), args=ocp.args.StandardRestore(state)
     )
+    print(f"Checkpoint restored from step {state.step}.")
+    
+    return state
 
+def prediction_fn(
+    configs,
+    dataset,
+    state
+    ):
+    
+    mask_windows = list(
+        zip(configs.masked_interval_starts, configs.masked_interval_ends)
+    )
+    
     # Loading Databases
-    test_data = list(batch_sampler(test_ds, mask_windows, shuffle=False, batch_size=1))
-
+    test_data = list(batch_sampler(dataset, mask_windows, shuffle=False, batch_size=1))
+    
     spectraformer_predictions = [
         predict(
             state.apply_fn,
@@ -94,17 +120,20 @@ def load_model(
         )
         for datapoint in test_data
     ]
-    return state, spectraformer_predictions
-
+    return spectraformer_predictions
 
 if __name__ == "__main__":
     
-    # Config file reading
     configs = ml_confs.from_file(config_file_path)
     configs.tabulate()
     
+    train_ds = preprocess_dataset(xr.load_dataarray(datadir / f"{configs.train_dataset}.nc"))
     
+    state = load_model(configs, dataset=train_ds)
     
-    
+    for elem in epath.Path(mixdir).iterdir():
+        dataset_elem = preprocess_dataset( xr.load_dataarray(elem) )
+        predictions = prediction_fn(configs, dataset_elem, state)
+        predictions.to_netcdf(mixdir / f"{elem}_unmixed.nc")
     
     print('Unmixing done.')
